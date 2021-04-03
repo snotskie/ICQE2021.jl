22328b8167ef1d413e798323ca76f59cdaa73483
Bryan A. Knowles <snotskie@gmail.com>
2021-04-03 10:30:32 -0500

    in the middle of refactoring

--- transitions/main.jl
+++ transitions/main.jl
@@ -11,10 +11,12 @@ using Dates
 using Colors
 using GLM
 using Clustering
+using HypothesisTests
 
 let # create a non-global scope
 
 # Prepare data
+cd(Base.source_dir())
 data = DataFrame(CSV.File("data/data.csv", normalizenames=true))
 data[!, :LABEL] = repeat(["No Label"], nrow(data))
 data[!, :DayScaled] = (data[!, :Day] .- minimum(data[!, :Day])) / (maximum(data[!, :Day]) - minimum(data[!, :Day]))
@@ -42,24 +44,23 @@ codes = [
     :NonHappy,
     :Sweets,
     :Oily,
-    # # :Friends, # qualitatively does not tell us anything
+    # :Friends, # qualitatively does not tell us anything
     :Out,
     :Doubt,
     :Cry,
     :Passed,
-    # # :Religion # only appears once in the data
+    # :Religion # only appears once in the data
 ]
 conversations = [:Day]
 units = [:Day]
 seed = 4321
+weight1 = 0.999999999999
 knn = 35
-weights1 = [0.0, 0.01, 0.5, 0.7]
-plotsizes = [7, 7, 20, 20]
-epsval = 0.5
+epsval = 0.9
 min_cluster_size=5
 min_neighbors=2
+plotsize = 25
 limses = [0.025, 0.05, 0.1]
-MRs = [(2, 6), (1, 5), (1, 3), (3, 4), (2, 4)]
 
 # Descriptive Statistics
 println("Descripive Statistics:")
@@ -68,18 +69,16 @@ for code in sort(codes)
 end
 
 # ENA
-ena = ENAModel(data, codes, conversations, units, dropEmpty=true)
+ena = ENAModel(data, codes, conversations, units, dropEmpty=true, sphereNormalize=true)
 p = plot(ena, weakLinks=false)
 savefig(p, "images/SVD.png")
 
 # UMAP
 ## Preprocessing unit, code, and "line" data for embedding
-centroidX = transpose(Matrix{Float64}(hcat(ena.metadata[!, :DayScaled]/maximum(ena.metadata[!, :DayScaled]), ena.centroidModel[!, ena.networkModel[!, :relationship]])))
-codeX = transpose(Matrix{Float64}(hcat([-9999 for row in eachrow(ena.codeModel)], ena.codeModel[!, ena.networkModel[!, :relationship]])))
+accumX = transpose(Matrix{Float64}(hcat(ena.metadata[!, :DayScaled]/maximum(ena.metadata[!, :DayScaled]), ena.accumModel[!, ena.networkModel[!, :relationship]])))
 lineX = nothing
 unitIndex = 0
-codeIndex = size(centroidX, 2)
-lineIndex = codeIndex + size(codeX, 2)
+lineIndex = unitIndex + size(accumX, 2)
 for (i, networkRow) in enumerate(eachrow(ena.networkModel))
     j, k = ena.relationshipMap[networkRow[:relationship]]
     pointA = Vector{Float64}([-9999, ena.codeModel[j, ena.networkModel[!, :relationship]]...])
@@ -101,13 +100,13 @@ function computeEmbedding(knn, weight1, seed)
     weightn = (1-weight1) / nrow(ena.networkModel)
     weights = [weight1, [weightn for row in eachrow(ena.networkModel)]...]
     metric = WeightedEuclidean(weights/sum(weights))
-    model = UMAP_(centroidX; n_neighbors=knn, min_dist=0.0000000001, metric=metric)
+    model = UMAP_(accumX; n_neighbors=knn, min_dist=0.0000000001, metric=metric)
 
     ### Project network components into the embedding
     weightn = 1 / nrow(ena.networkModel)
     weights = [0, [weightn for row in eachrow(ena.networkModel)]...]
     metric = WeightedEuclidean(weights/sum(weights))
-    networkEmbedding = UMAP.transform(model, hcat(codeX, lineX); n_neighbors=1, min_dist=0.0000000001, metric=metric)
+    networkEmbedding = UMAP.transform(model, lineX; n_neighbors=1, min_dist=0.0000000001, metric=metric)
 
     ### Unseed
     Random.seed!(Dates.value(now()))
@@ -130,10 +129,19 @@ function plotUMAP(embedding, plotsize, colormode)
     yticks!(p, [-plotsize, plotsize])
     xlims!(p, (-plotsize, plotsize))
     ylims!(p, (-plotsize, plotsize))
-    meanX = mean(embedding[1, 1:codeIndex])
-    meanY = mean(embedding[2, 1:codeIndex])
 
-    displayRows = repeat([true], nrow(ena.centroidModel))
+    displayRows = map(eachrow(ena.accumModel)) do row
+        if sum(row[ena.networkModel[!, :relationship]]) == 0
+            return false
+        else
+            return true
+        end
+    end
+    
+    displayRows2 = [displayRows..., repeat([false], size(embedding, 2) - length(displayRows))...]
+    meanX = mean(embedding[1, displayRows2])
+    meanY = mean(embedding[2, displayRows2])
+
     displayAccums = ena.accumModel[displayRows, :]
     lineWidths = map(eachrow(ena.networkModel)) do networkRow
         return sum(displayAccums[!, networkRow[:relationship]])
@@ -150,8 +158,6 @@ function plotUMAP(embedding, plotsize, colormode)
     codeYs = zeros(nrow(ena.codeModel))
     for (i, networkRow) in enumerate(eachrow(ena.networkModel))
         j, k = ena.relationshipMap[networkRow[:relationship]]
-        # pointA = [embedding[1, codeIndex+j], embedding[2, codeIndex+j]]
-        # pointB = [embedding[1, codeIndex+k], embedding[2, codeIndex+k]]
         pointT = [embedding[1, lineIndex+i], embedding[2, lineIndex+i]]
         codeXs[j] += pointT[1] * lineWidths[i] / codeWidths[j]
         codeXs[k] += pointT[1] * lineWidths[i] / codeWidths[k]
@@ -162,8 +168,6 @@ function plotUMAP(embedding, plotsize, colormode)
     lineWidths *= 8 / maximum(allLineWidths)
     for (i, networkRow) in enumerate(eachrow(ena.networkModel))
         j, k = ena.relationshipMap[networkRow[:relationship]]
-        # pointA = [embedding[1, codeIndex+j], embedding[2, codeIndex+j]]
-        # pointB = [embedding[1, codeIndex+k], embedding[2, codeIndex+k]]
         pointA = [codeXs[j], codeYs[j]]
         pointB = [codeXs[k], codeYs[k]]
         pointT = [embedding[1, lineIndex+i], embedding[2, lineIndex+i]]
@@ -180,8 +184,6 @@ function plotUMAP(embedding, plotsize, colormode)
     codeWidths *= 8 / maximum(codeWidths)
     labels = map(label->text(label, :top, 8), ena.codeModel[!, :code])
     plot!(p,
-        # embedding[1, (codeIndex+1):lineIndex] .- meanX,
-        # embedding[2, (codeIndex+1):lineIndex] .- meanY,
         codeXs .- meanX,
         codeYs .- meanY,
         label=nothing,
@@ -203,7 +205,6 @@ function plotUMAP(embedding, plotsize, colormode)
         end
     end
     
-    displayRows2 = [displayRows..., repeat([false], size(embedding, 2) - length(displayRows))...]
     plot!(p,
         embedding[1, displayRows2] .- meanX,
         embedding[2, displayRows2] .- meanY,
@@ -230,17 +231,12 @@ function plotUMAP(embedding, plotsize, colormode)
     return p
 end
 
-## Computing and plotting each desired embedding, ending with the one we'll continue to use below
-embedding = nothing
-for (weight1, plotsize) in zip(weights1, plotsizes)
-    embedding = computeEmbedding(knn, weight1, seed)
-    p = plotUMAP(embedding, plotsize, :spectral)
-    savefig(p, "images/SpectralUMAP_$(weight1)_$(plotsize).png")
-end
+# Optmization - Computing embeddings, monte carlo optimizing for lowest-yet-discriminating weight1
+embedding = computeEmbedding(knn, weight1, seed)
 
-# DBSCAN
-## Auto-labeling by detected cluster
-results = dbscan(embedding[1:2, 1:codeIndex], epsval; min_cluster_size=min_cluster_size, min_neighbors=min_neighbors)
+## DBSCAN - Auto-labeling by detected cluster
+ena.metadata[!, :LABEL] .= "No Label"
+results = dbscan(embedding[1:2, 1:lineIndex], epsval; min_cluster_size=min_cluster_size, min_neighbors=min_neighbors)
 for (i, cluster) in enumerate(results)
     mylabel = "Auto Cluster #$(i)"
     ena.metadata[cluster.core_indices, :LABEL] .= mylabel
@@ -254,18 +250,20 @@ for (i, cluster) in enumerate(results)
     end
 end
 
-## Plotting result
-p = plotUMAP(embedding, plotsizes[end], :label)
-savefig(p, "images/LabelUMAP.png")
-
-## Saving back into dataframe
+## Saving labels back into dataframe
 nonEmptyRows = map(eachrow(data)) do row
     return sum(row[codes]) > 1
 end
 
 data[nonEmptyRows, :LABEL] = ena.metadata[!, :LABEL]
 
-## Displaying time windows
+## Plotting result, both spectral and clustered
+p = plotUMAP(embedding, plotsize, :spectral)
+savefig(p, "images/SpectralUMAP_$(weight1)_$(plotsize).png")
+p = plotUMAP(embedding, plotsize, :label)
+savefig(p, "images/LabelUMAP.png")
+
+## Displaying time windows (text)
 for label in sort(unique(data[!, :LABEL]))
     if label != "No Label"
         labelRows = data[!, :LABEL] .== label
@@ -273,27 +271,51 @@ for label in sort(unique(data[!, :LABEL]))
     end
 end
 
+## Displaying time windows (cdf's)
+dayLabelMap = Dict(row[:Day] => row[:LABEL] for row in eachrow(data))
+labelCounts = Dict(label => 0 for label in unique(data[!, :LABEL]))
+labelPrevX = Dict(label => 0 for label in unique(data[!, :LABEL]))
+p = plot(; size=(800,800))
+for (x, day) in enumerate(data[!, :Day])
+    label = dayLabelMap[day]
+    if label != "No Label"
+        plot!(p,
+            [labelPrevX[label], x],
+            [labelCounts[label], labelCounts[label] + 1],
+            label=nothing,
+            seriestype=:line,
+            linecolor=colorMap[label])
+
+        labelCounts[label] += 1
+        labelPrevX[label] = x
+    end
+end
+
+savefig(p, "images/CDFs.png")
+
 # Code and Count
 agg_data = combine(groupby(data, :LABEL), sort(codes) .=> sum .=> sort(codes))
 display(agg_data)
 CSV.write("data/agg_data.csv", agg_data)
 
-# LDA
-groups = sort(unique(data[!, :LABEL]))
-for dim1 in 1:(length(groups)-3)
-    ## Run and plot LDA for all nodes
-    rotation = LDARotation(:LABEL, dim1)
-    ena = ENAModel(data, codes, conversations, units, dropEmpty=true, rotateBy=rotation, subsetFilter=x->x[:LABEL]!="No Label")
-    for lims in limses
-        p = plot(ena, weakLinks=false, showUnits=false, lims=lims)    
-        savefig(p, "images/LDA$(dim1)-$(lims).png")
-    end
-end
+# # LDA
+# groups = sort(unique(data[!, :LABEL]))
+# for dim1 in 1:(length(groups)-3)
+#     ## Run and plot LDA for all nodes
+#     rotation = LDARotation(:LABEL, dim1)
+#     ena = ENAModel(data, codes, conversations, units, dropEmpty=true, rotateBy=rotation, subsetFilter=x->x[:LABEL]!="No Label")
+#     for lims in limses
+#         p = plot(ena, weakLinks=false, showUnits=false, lims=lims)    
+#         savefig(p, "images/LDA$(dim1)-$(lims).png")
+#     end
+# end
 
 # MR
-for (group1, group2) in MRs
+groups = sort(unique(data[!, :LABEL]))
+for group1 in 1:(length(groups)-2)
+    group2 = group1 + 1
     rotation = MeansRotation(:LABEL, "Auto Cluster #$(group2)", "Auto Cluster #$(group1)")
-    ena = ENAModel(data, codes, conversations, units, dropEmpty=true, rotateBy=rotation, subsetFilter=x->x[:LABEL]!="No Label")
+    ena = ENAModel(data, codes, conversations, units, dropEmpty=true, rotateBy=rotation, subsetFilter=x->x[:LABEL]!="No Label") # TODO fix this
     p = plot(ena, weakLinks=false)
     savefig(p, "images/MR_$(group1)_$(group2).png")
     # TODO run mann whitney tests, and pull out and report the coregistrations
